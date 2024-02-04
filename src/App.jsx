2bc165b11e44490e182983063701193eb70113b3
Gianclaudio <gianclaudiotarantini@gmail.com>
2024-02-04 20:30:02 +0100

    Create pagine e rotte

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/SignIn" element={<SignIn />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Profile" element={<Profile />} />
+        </Route>
+        
+      </Routes>
+    
+    </BrowserRouter>
   )
 }
 
