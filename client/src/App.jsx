07f5ffe90025b9da274a223e9a08c7ac42a60fab
Gianclaudio <gianclaudiotarantini@gmail.com>
2024-02-18 20:50:25 +0100

    Creata pagina privata solo con accesso + immagine navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from "./pages/SignUp"
 import About from "./pages/About"
 import Profile from "./pages/Profile"
 import Header from "./components/Header"
+import PrivateRoute from "./components/PrivateRoute"
 
 export default function App() {
   return (
@@ -17,6 +18,9 @@ export default function App() {
           <Route path="/Sign-In" element={<SignIn />} />
           <Route path="/Sign-Up" element={<SignUp />} />
           <Route path="/About" element={<About />} />
+          <Route element={<PrivateRoute />} >
+            <Route path="/Profile" element={<Profile />} />
+          </Route>
           <Route path="/Profile" element={<Profile />} />
         </Route>
         
