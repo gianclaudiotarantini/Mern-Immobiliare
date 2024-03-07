b021d382653e77b1e06b2ae7475ca0cc4b31e637
Gianclaudio <gianclaudiotarantini@gmail.com>
2024-03-07 20:39:09 +0100

    Completata pagina creazione annuncio

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -5,6 +5,7 @@ export const createListing = async (req, res, next) => {
     const listing = await Listing.create(req.body);
     return res.status(201).json(listing);
   } catch (error) {
+    console.error("Errore durante la creazione dell'annuncio:", error);
     next(error);
   }
 };
--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -8,15 +8,31 @@ import {
   uploadBytesResumable,
 } from 'firebase/storage';
 import { app } from '../firebase';
-import { set } from 'mongoose';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 export default function CreateListing() {
+  const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
   const [files, setFiles] = useState([]);
   const [formData, setFormData] = useState({
     imageUrls: [],
+    nome: '',
+    descrizione: '',
+    indirizzo: '',
+    tipo: 'affitto',
+    camereDaLetto: 1,
+    bagni: 1,
+    prezzoRegolare: 50,
+    prezzoScontato: 0,
+    offerta: false,
+    parcheggio: false,
+    arredata: false,
   });
   const [imageUploadError, setImageUploadError] = useState(false);
   const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   console.log(formData);
   const handleImageSubmit = (e) => {
     if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
@@ -37,12 +53,74 @@ export default function CreateListing() {
           setUploading(false);
         })
         .catch((err) => {
-          setImageUploadError('Image upload failed (2 mb max per image)');
+          setImageUploadError('Caricamento immagine non riuscito (2 mb massimo per immagine)');
           setUploading(false);
         });
     } else {
-      setImageUploadError('You can only upload 6 images per listing');
+      setImageUploadError('Puoi inviare fino a 6 immagini per annuncio');
       setUploading(false);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.id === 'vendita' || e.target.id === 'affitto') {
+      setFormData({
+        ...formData,
+        type: e.target.id,
+      });
+    }
+
+    if (
+      e.target.id === 'parcheggio' ||
+      e.target.id === 'arredata' ||
+      e.target.id === 'offerta'
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.checked,
+      });
+    }
+
+    if (
+      e.target.type === 'number' ||
+      e.target.type === 'text' ||
+      e.target.type === 'textarea'
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (formData.imageUrls.length < 1)
+        return setError('Devi caricare almeno un immagine');
+      if (+formData.regularPrice < +formData.discountPrice)
+        return setError('Il prezzo scontato deve essere inferiore al prezzo regolare');
+      setLoading(true);
+      setError(false);
+      const res = await fetch('/api/listing/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...formData,
+          riferimentoUtente: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(data.message);
+      }
+      navigate(`/listing/${data._id}`);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
     }
   };
 
@@ -82,7 +160,7 @@ export default function CreateListing() {
       <h1 className='text-3xl font-semibold text-center my-7'>
         Crea un Annuncio
       </h1>
-      <form className='flex flex-col sm:flex-row gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col sm:flex-row gap-4'>
         <div className='flex flex-col gap-4 flex-1'>
           <input
             type='text'
@@ -92,6 +170,8 @@ export default function CreateListing() {
             maxLength='62'
             minLength='10'
             required
+            onChange={handleChange}
+            value={formData.name}
           />
           <textarea
             type='text'
@@ -99,6 +179,8 @@ export default function CreateListing() {
             className='border p-3 rounded-lg'
             id='descrizione'
             required
+            onChange={handleChange}
+            value={formData.descrizione}
           />
           <input
             type='text'
@@ -106,26 +188,28 @@ export default function CreateListing() {
             className='border p-3 rounded-lg'
             id='indirizzo'
             required
+            onChange={handleChange}
+            value={formData.address}
           />
           <div className='flex gap-6 flex-wrap'>
             <div className='flex gap-2'>
-              <input type='checkbox' id='Vendere' className='w-5' />
+              <input type='checkbox' id='vendita' className='w-5' onChange={handleChange}  checked={formData.type === 'vendita'}/>
               <span>Vendita</span>
             </div>
             <div className='flex gap-2'>
-              <input type='checkbox' id='Affitto' className='w-5' />
+              <input type='checkbox' id='affitto' className='w-5'  onChange={handleChange} checked={formData.type === 'affitto'} />
               <span>Affitto</span>
             </div>
             <div className='flex gap-2'>
-              <input type='checkbox' id='Parcheggio' className='w-5' />
+              <input type='checkbox' id='parcheggio' className='w-5'  onChange={handleChange} checked={formData.parcheggio} />
               <span>Parcheggio</span>
             </div>
             <div className='flex gap-2'>
-              <input type='checkbox' id='Arredata' className='w-5' />
+              <input type='checkbox' id='arredata' className='w-5'  onChange={handleChange} checked={formData.arredata} />
               <span>Arredata</span>
             </div>
             <div className='flex gap-2'>
-              <input type='checkbox' id='Offerta' className='w-5' />
+              <input type='checkbox' id='offerta' className='w-5' onChange={handleChange} checked={formData.offerta} />
               <span>Offerta</span>
             </div>
           </div>
@@ -138,6 +222,8 @@ export default function CreateListing() {
                 max='10'
                 required
                 className='p-3 border border-gray-300 rounded-lg'
+                onChange={handleChange}
+                value={formData.camereDaLetto}
               />
               <p>Letti</p>
             </div>
@@ -149,6 +235,8 @@ export default function CreateListing() {
                 max='10'
                 required
                 className='p-3 border border-gray-300 rounded-lg'
+                onChange={handleChange}
+                value={formData.bagni}
               />
               <p>Bagni</p>
             </div>
@@ -156,30 +244,36 @@ export default function CreateListing() {
               <input
                 type='number'
                 id='prezzoRegolare'
-                min='1'
-                max='10'
+                min='50'
+                max='1000000'
                 required
                 className='p-3 border border-gray-300 rounded-lg'
+                onChange={handleChange}
+                value={formData.prezzoRegolare}
               />
               <div className='flex flex-col items-center'>
                 <p>Prezzo Regolare</p>
                 <span className='text-xs'>(€ / mese)</span>
               </div>
             </div>
+            {formData.offerta && (
             <div className='flex items-center gap-2'>
               <input
                 type='number'
                 id='prezzoScontato'
-                min='1'
-                max='10'
+                min='0'
+                max='10000000'
                 required
                 className='p-3 border border-gray-300 rounded-lg'
+                onChange={handleChange}
+                  value={formData.prezzoScontato}
               />
               <div className='flex flex-col items-center'>
                 <p>Prezzo Scontato</p>
                 <span className='text-xs'>(€ / mese)</span>
               </div>
             </div>
+            )}
           </div>
         </div>
         <div className="flex flex-col flex-1 gap-4">
@@ -188,7 +282,7 @@ export default function CreateListing() {
           </p>
           <div className="flex gap-4">
             <input onChange={(e) => setFiles(e.target.files)} className='p-3 border border-gray-300 rounded w-full' type="file" id='images' accept='image/*' multiple />
-            <button type='button' disabled={uploading} onClick={handleImageSubmit} className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'> {uploading ? 'Uploading...' : 'Upload'}</button>
+            <button disabled={uploading} type='button' onClick={handleImageSubmit} className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'> {uploading ? 'Uploading...' : 'Upload'}</button>
           </div>
         
         <p className='text-red-700 text-sm'>
@@ -214,9 +308,13 @@ export default function CreateListing() {
                 </button>
               </div>
             ))}
-          <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-            Crea Annuncio
+          <button
+            disabled={loading || uploading}
+            className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+          >
+            {loading ? 'Creazione...' : 'Crea Annuncio'}
           </button>
+          {error && <p className='text-red-700 text-sm'>{error}</p>}
         </div>
       </form>
     </main>
