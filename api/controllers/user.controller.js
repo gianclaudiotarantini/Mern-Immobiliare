7b287336768275cf5d8182b14c412c7e192c1ec1
Gianclaudio <gianclaudiotarantini@gmail.com>
2024-03-10 20:33:36 +0100

    Creata route api listings

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
+import Listing from '../models/listing.model.js';
 
 
 export const test = (req, res) => {
@@ -49,6 +50,21 @@ export const updateUser = async (req, res, next) => {
       next(error);
     }
   };
+
+  export const getUserListings = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+      try {
+        const listings = await Listing.find({ riferimentoUtente: req.params.id });
+        res.status(200).json(listings);
+      } catch (error) {
+        next(error);
+      }
+    } else {
+      return next(errorHandler(401, ' Tu puoi accedere solo al tuo account'));
+    }
+  };
+  
+
 
 
 
