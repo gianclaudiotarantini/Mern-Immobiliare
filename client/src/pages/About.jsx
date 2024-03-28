41c5401a2dd3029a16b241e4aae673c5efb48dfc
Gianclaudio <gianclaudiotarantini@gmail.com>
2024-03-28 18:44:45 +0100

    Completata pagina Chi siamo

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,31 @@
 export default function About() {
   return (
-    <div>
-      About
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+         <span className="text-slate-500 font-bold">Chi Siamo</span>
+      </h1>
+      <p className="mb-4 text-slate-700">
+        GtImmobiliare è un agenzia immobiliare leader specializzata nell aiutare
+        i clienti ad acquistare, vendere e affittare immobili nelle zone più
+        desiderabili. Il nostro team di agenti esperti è dedicato a fornire
+        servizio eccezionale e rendere il processo di acquisto e vendita
+        altrettanto fluido possibile.
+      </p>
+      <p className="mb-4 text-slate-700">
+        La nostra missione è aiutare i nostri clienti a raggiungere i loro
+        obiettivi immobiliari fornendo consulenza di esperti, servizio
+        personalizzato e una profonda comprensione del mercato locale. Che tu
+        stia cercando di acquistare, vendere o affittare una proprietà, siamo
+        qui per aiutarti in ogni fase del processo.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Il nostro team di agenti ha una vasta esperienza e conoscenza nel
+        settore reale settore immobiliare e ci impegniamo a fornire il massimo
+        livello di servizio ai nostri clienti. Crediamo che acquistare o vendere
+        un immobile dovrebbe essere un esperienza emozionante e gratificante e
+        ci dedichiamo a farlo rendendolo una realtà per ognuno dei nostri
+        clienti.
+      </p>
     </div>
-  )
+  );
 }
